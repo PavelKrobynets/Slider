8fce5bc5125676a4d518d4bdd6154d5642d33a92
Pavel <krobynetspavel@gmail.com>
2024-05-16 18:37:40 +0300

    slider done

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,37 @@
 "use strict";
 
-
-
 window.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper('.swiper', {
-		// Optional parameters
-		direction: 'horizontal',
-		loop: true,
-		speed: 1600,
-		mousewheel: { },
-	
-		// If we need pagination
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-		},
-	
-		// Navigation arrows
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-	});
+  const video = document.querySelector(".video-background");
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    speed: 1600,
+    mousewheel: {},
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  swiper.on("slideChange", function () {
+    gsap.to(video, 1.6, {
+      currentTime: (video.duration / (this.slides.length - 1)) * this.realIndex,
+      ease: Power2.easeOut,
+    });
+  });
+  swiper
+    .on("slideChangeTransitionStart", function () {
+      video.classList.add("change");
+    })
+    .on("slideChangeTransitionEnd", function () {
+      video.classList.remove("change");
+    });
 });
